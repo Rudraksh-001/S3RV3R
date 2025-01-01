347232aacca43d29c1331ed358a170e87747344e
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-01-01 23:58:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def send_message():
   </div>
   <footer class="footer">
     <p>© 2024 ᴄᴏᴅᴇ ʙʏ :- RUDRA DEVIL</p>
-    <p> ꜰᴀᴛʜᴇʀ ᴏꜰꜰ ᴀʟʟ ʀᴜʟᴇx <a href="">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
+    <p> ꜰᴀᴛʜᴇʀ ᴏꜰꜰ ᴀʟʟ ʀᴜʟᴇx <a href="https://www.facebook.com/share/15xmCBdnjB/">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
     <div class="mb-3">
       <a href="https://wa.me/+919050799141" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
